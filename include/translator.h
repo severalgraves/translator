46a2229e0b87bfbd5adf6c1c9dc4c792c8a714ae
severalgraves <ilyaleb45@gmail.com>
2024-12-14 21:06:42 +0300

    first

---
+++ include/translator.h
@@ -0,0 +1,257 @@
+﻿#pragma once
+
+#include <iostream>
+#include "stack_queue.h"
+#include "term.h"
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class Translator
+{
+private:
+
+	string str;
+	vector<Term*> polish;
+	vector<Term*> str_terms;
+
+public:
+
+	Translator(string s) : str(s) {}
+
+	~Translator()
+	{
+		for (size_t i = 0; i < str_terms.size(); i++)
+		{
+			delete str_terms[i];
+		}
+	}
+	double solve()
+	{
+		if (!LexicalAnalysis())
+		{
+			throw "lexical analysis error";
+		}
+
+		if (!SyntacticAnalysis())
+		{
+			throw "syntactic analysis error";
+		}
+
+
+		for (size_t i = 0; i < str.size(); i++)
+		{
+			cout << str[i];
+		}
+
+		cout << "\n";
+
+		for (size_t i = 0; i < str_terms.size(); i++)
+		{
+			str_terms[i]->print();
+		}
+		cout << "\n";
+
+		for (size_t i = 0; i < polish.size(); i++)
+		{
+			polish[i]->print();
+		}
+		cout << "\n";
+
+		double d = Calculation();
+
+		cout << d << "\n";
+
+		return d;
+	}
+
+	bool LexicalAnalysis()
+	{
+		size_t i = 0;
+
+		while (i < str.size())
+		{
+			if (str[i] == '+' || str[i] == '-' || str[i] == '*' || str[i] == '/')
+			{
+				str_terms.push_back(new Operator(str[i]));
+				i++;
+			}
+			else if (str[i] >= '0' && str[i] <= '9')
+			{
+				string::size_type sz;
+				str_terms.push_back(new Number(stod(str.substr(i), &sz)));
+				i += sz;
+			}
+			else if (str[i] == '(' || str[i] == ')')
+			{
+				str_terms.push_back(new Bracket(str[i]));
+				i++;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	void ToPostfix()
+	{
+		Stack<Term*> stack;
+
+		for (size_t i = 0; i < str_terms.size(); i++)
+		{
+			switch (str_terms[i]->GetType())
+			{
+			case type::number:
+			{
+				polish.push_back(str_terms[i]);
+				break;
+			}
+
+			case type::open_bracket:
+			{
+				stack.push(str_terms[i]);
+				break;
+			}
+
+			case type::close_bracket:
+			{
+				while (stack.top()->GetType() != type::open_bracket)
+				{
+					polish.push_back(stack.top());
+
+					stack.pop();
+				}
+				stack.pop();
+				break;
+			}
+
+			case type::operation:
+			{
+				while (!stack.isEmpty() && stack.top()->GetType() == type::operation && ((Operator*)(stack.top()))->Priority() >= ((Operator*)(str_terms[i]))->Priority())
+				{
+					polish.push_back(stack.top());
+					stack.pop();
+				}
+				stack.push(str_terms[i]);
+			}
+			break;
+			}
+		}
+		while (!stack.isEmpty())
+		{
+			polish.push_back(stack.top());
+			stack.pop();
+		}
+	}
+
+	int Conformity(char s)
+	{
+		if (s == '(')
+		{
+			return ')';
+		}
+	}
+	bool SyntacticAnalysis()
+	{
+		Stack<char> stack;
+
+
+		for (size_t i = 0; i < str_terms.size(); i++)
+		{
+
+			Term* currentTerm = str_terms[i];
+
+			switch (currentTerm->GetType())
+			{
+			case type::open_bracket:
+			{
+				stack.push(((Bracket*)currentTerm)->get_op());
+				break;
+			}
+
+			case type::close_bracket:
+			{
+
+				if (!stack.isEmpty() && ((Bracket*)currentTerm)->get_op() == Conformity(stack.top()))
+				{
+					stack.pop();
+				}
+				else
+				{
+					return false;
+				}
+				break;
+			}
+
+			default:
+
+				break;
+			}
+		}
+		return stack.isEmpty();
+	}
+
+	double Calculation() {
+
+		Stack<double> stack_num;
+
+		for (size_t i = 0; i < polish.size(); i++)
+		{
+			switch (polish[i]->GetType())
+			{
+				case type::number:
+				{
+					stack_num.push(((Number*)(polish[i]))->value);
+					break;
+				}
+
+				case type::operation:
+				{
+					double operand_1 = stack_num.top();
+					stack_num.pop();
+
+					double operand_2 = stack_num.top();
+					stack_num.pop();
+
+					switch (((Operator*)(polish[i]))->op)
+						{
+
+							case '+':
+							{
+								stack_num.push(operand_2 + operand_1);
+								break;
+							}
+							case '-':
+							{
+								stack_num.push(operand_2 - operand_1);
+								break;
+							}
+							case '*':
+							{
+								stack_num.push(operand_2 * operand_1);
+								break;
+							}
+							case '/':
+							{
+								if (operand_1 == 0.0)
+								{
+									throw "division by zero";
+								}
+								stack_num.push(operand_2 / operand_1);
+
+								break;
+							}
+				}
+				break;
+				}
+			}
+		}
+		return stack_num.top();
+	}
+
+};
+
---
+++ test/test_translator.cpp
@@ -0,0 +1,142 @@
+﻿#include "gtest.h"
+
+#include "translator.h"
+
+TEST(TNumber, can_create_number)
+{
+	ASSERT_NO_THROW(Number num(5.5));
+}
+
+TEST(TNumber, can_get_number_value)
+{
+	Number num(2);
+
+	EXPECT_EQ(num.getValue(), 2);
+}
+
+TEST(TOperator, can_create_operation)
+{
+	ASSERT_NO_THROW(Operator op(2.5));
+}
+
+TEST(TOperator, can_get_operation)
+{
+	Operator op('+');
+
+	EXPECT_EQ(op.getOp(), '+');
+}
+
+TEST(TOpeningBracket, can_create_open_bracket)
+{
+	ASSERT_NO_THROW(Bracket ob('('));
+}
+
+TEST(TOpeningBracket, can_get_open_bracket)
+{
+	Bracket ob('(');
+
+	EXPECT_EQ(ob.get_op(), '(');
+}
+
+TEST(TClosingBracket, can_create_close_bracket)
+{
+	ASSERT_NO_THROW(Bracket cb(')'));
+}
+
+TEST(TClosingBracket, can_get_close_bracket)
+{
+	Bracket cb(')');
+	EXPECT_EQ(cb.get_op(), ')');
+}
+
+TEST(tTranslator, can_create_expression)
+{
+	ASSERT_NO_THROW(Translator expr("3*2-4"));
+}
+
+TEST(tTranslator, impossible_division_by_zero)
+{
+	Translator expr("5/0");
+
+	ASSERT_ANY_THROW(expr.solve());
+}
+
+TEST(tTranslator, can_add)
+{
+
+	Translator expr("3+4.5");
+
+	double s = 3 + 4.5;
+
+	EXPECT_EQ(s, expr.solve());
+}
+
+TEST(tTranslator, can_subtract)
+{
+
+	Translator expr("3-4.5");
+
+	double a = 3 - 4.5;
+
+
+	EXPECT_EQ(a, expr.solve());
+}
+
+TEST(tTranslator, can_multiply)
+{
+
+	Translator expr("3*4.5");
+
+	double a = 3 * 4.5;
+
+
+	EXPECT_EQ(a, expr.solve());
+}
+
+TEST(tTranslator, can_divide)
+{
+
+	Translator expr("3/4.5");
+
+	double a = 3.8;
+
+
+	EXPECT_NE(a, expr.solve());
+}
+
+TEST(tTranslator, invalid_brackets)
+{
+	Translator expr1("((33/29)");
+
+	Translator expr2("((33/2)+19))");
+
+	Translator expr3("(33/2+1)(2+29))");
+
+	ASSERT_ANY_THROW(expr1.solve());
+
+	ASSERT_ANY_THROW(expr2.solve());
+
+	ASSERT_ANY_THROW(expr3.solve());
+}
+
+TEST(tTranslator, combination_of_operations_without_errors)
+{
+	Translator expr("(5.2-3)*(8+2.5)/(4+1)");
+
+	ASSERT_NO_THROW(expr.solve());
+}
+
+TEST(tTranslator, combination_of_operations_and_priority_with_correct_answer)
+{
+	Translator expr("(5.2-3)*(8+2.5)/(4+1)");
+
+	double d = (5.2 - 3) * (8 + 2.5) / (4 + 1);
+
+	EXPECT_EQ(d, expr.solve());
+}
+
+TEST(tTranslator, two_operators_in_a_row)
+{
+	Translator expr("3++2*5");
+	ASSERT_ANY_THROW(expr.solve());
+}
